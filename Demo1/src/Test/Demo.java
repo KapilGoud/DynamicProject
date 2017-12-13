64669cdbbf0a72cf6503e55be1259eb9ac6f0c4b
KapilGoud <kapil.goud14@gmail.com>
2017-12-13 10:51:40 +0530

    change

--- Demo1/src/Test/Demo.java
+++ Demo1/src/Test/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		for (int i = 0; i <10; i++) {
 			System.out.println("This is Dynamic Web Project");
+			System.out.println("change 13/12/2017");
+			
 		}
 
 	}
